368262f2e8d01751769c2470fb87a0dcc238ab48
= <arpittyagi389@gmail.com>
2026-05-31 19:27:56 +0530

    Correct responseEntity

--- backend/event-service/src/main/java/com/streamanalytics/event_service/controller/LocationController.java
+++ backend/event-service/src/main/java/com/streamanalytics/event_service/controller/LocationController.java
@@ -1,24 +1,24 @@
 package com.streamanalytics.event_service.controller;
 import com.streamanalytics.event_service.service.KafkaService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/location")
 public class LocationController {
 
-    KafkaService kafkaService;
+    private final KafkaService kafkaService;
 
     public LocationController(KafkaService kafkaService) {
         this.kafkaService = kafkaService;
     }
 
-    @PostMapping("/send")
-    public ResponseEntity<?> updatedLocation(String location){
-        kafkaService.updatedLocation("location updated");
-        return new ResponseEntity<>(HttpStatus.OK);
+    @PostMapping("/update")
+    public ResponseEntity<String> updatedLocation(@RequestParam String location){
+        kafkaService.updatedLocation(location);
+        return ResponseEntity.accepted().body("Location Updated");
     }
 }
