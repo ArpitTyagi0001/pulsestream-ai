3bf7fe556c97fcdf9b032576126f395bf65e22a0
= <arpittyagi389@gmail.com>
2026-05-31 19:25:51 +0530

    make a construction based dependency and correct logic

--- backend/event-service/src/main/java/com/streamanalytics/event_service/service/KafkaService.java
+++ backend/event-service/src/main/java/com/streamanalytics/event_service/service/KafkaService.java
@@ -1,18 +1,19 @@
 package com.streamanalytics.event_service.service;
 
 import com.streamanalytics.event_service.config.AppConstants;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaService {
 
-    @Autowired
-    KafkaTemplate<String , String> kafkaTemplate;
+    private final KafkaTemplate<String , String> kafkaTemplate;
 
-    public boolean updatedLocation(String location){
+    public KafkaService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void updatedLocation(String location){
         kafkaTemplate.send(AppConstants.Location_Topic_Name , location);
-        return true;
     }
 }
