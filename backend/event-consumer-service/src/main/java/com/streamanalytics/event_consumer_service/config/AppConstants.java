ad5e525397ab107cfd965ceaa19645c7199afcb1
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-06 18:33:49 +0530

    Add kafka config

---
+++ backend/event-consumer-service/src/main/java/com/streamanalytics/event_consumer_service/config/AppConstants.java
@@ -0,0 +1,6 @@
+package com.streamanalytics.event_consumer_service.config;
+
+public final class AppConstants {
+    public static final String LOCATION_TOPIC_NAME = "location_topic";
+    public static final String GROUP_ID = "group1";
+}
