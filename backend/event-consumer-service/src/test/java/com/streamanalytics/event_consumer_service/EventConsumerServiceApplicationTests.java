da4f713375d1c4f400621fd4031d2502bf8eb6d1
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-06 16:41:02 +0530

    Add event consumer service

---
+++ backend/event-consumer-service/src/test/java/com/streamanalytics/event_consumer_service/EventConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamanalytics.event_consumer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
