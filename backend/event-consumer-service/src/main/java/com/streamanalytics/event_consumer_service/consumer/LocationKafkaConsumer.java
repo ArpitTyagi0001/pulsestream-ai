a9ad0a724e9eae9f6747fb2fc479833e1ae0b3fe
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-09 21:41:45 +0530

    Implement Event Service to save data into a database

--- backend/event-consumer-service/src/main/java/com/streamanalytics/event_consumer_service/consumer/LocationKafkaConsumer.java
+++ backend/event-consumer-service/src/main/java/com/streamanalytics/event_consumer_service/consumer/LocationKafkaConsumer.java
@@ -1,26 +1,21 @@
 package com.streamanalytics.event_consumer_service.consumer;
 
 import com.streamanalytics.event_consumer_service.config.AppConstants;
-import com.streamanalytics.event_consumer_service.entity.Event;
-import com.streamanalytics.event_consumer_service.repo.EventRepo;
+import com.streamanalytics.event_consumer_service.service.EventService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LocationKafkaConsumer {
 
-    private EventRepo eventRepo;
+    private EventService eventService;
 
-    public LocationKafkaConsumer(EventRepo eventRepo) {
-        this.eventRepo = eventRepo;
+    public LocationKafkaConsumer(EventService eventService) {
+        this.eventService = eventService;
     }
 
     @KafkaListener(topics = AppConstants.LOCATION_TOPIC_NAME , groupId = AppConstants.GROUP_ID)
     public void listen(String location){
-        Event event = new Event();
-        event.setLocation(location);
-        event.setEventAt(java.time.LocalDateTime.now());
-
-        eventRepo.save(event);
+       eventService.SaveEvent(location);
     }
 }
