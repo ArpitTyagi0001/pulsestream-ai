99132972f177e7674e63d9abbcd3070c1f754e53
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-13 21:20:44 +0530

    Add controller layer

---
+++ backend/query_service/src/main/java/com/streamanalytics/query_service/controller/EventController.java
@@ -0,0 +1,9 @@
+package com.streamanalytics.query_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/track")
+public class EventController {
+}
