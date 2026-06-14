7916cd679e670af3b4d57fa9c344d483a1646934
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-14 21:20:05 +0530

    Add GetMapping Request

--- backend/query_service/src/main/java/com/streamanalytics/query_service/controller/EventController.java
+++ backend/query_service/src/main/java/com/streamanalytics/query_service/controller/EventController.java
@@ -1,9 +1,30 @@
 package com.streamanalytics.query_service.controller;
 
+import com.streamanalytics.query_service.entity.Event;
+import com.streamanalytics.query_service.service.EventService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/track")
+@RequestMapping("/api/events")
 public class EventController {
+    private EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/all")
+    public List<Event> getAllEvent(){
+        return eventService.getAllEvent();
+    }
+
+    @GetMapping("/get/{EventId}")
+    public Event getEventById(@PathVariable Long EventId){
+        return eventService.EventById(EventId);
+    }
 }
