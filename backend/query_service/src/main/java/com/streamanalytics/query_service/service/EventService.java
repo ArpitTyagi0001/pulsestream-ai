1376a8d2c7bedd61c9c1c2ae0426c97c8f8fe854
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-14 21:21:06 +0530

    Add Redis Annotion on Controller command to Data Fast Retrieve

--- backend/query_service/src/main/java/com/streamanalytics/query_service/service/EventService.java
+++ backend/query_service/src/main/java/com/streamanalytics/query_service/service/EventService.java
@@ -1,7 +1,28 @@
 package com.streamanalytics.query_service.service;
 
+import com.streamanalytics.query_service.entity.Event;
+import com.streamanalytics.query_service.repo.EventRepo;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EventService {
+    private EventRepo eventRepo;
+
+    public EventService(EventRepo eventRepo) {
+        this.eventRepo = eventRepo;
+    }
+
+    @Cacheable(value = "events")
+    public List<Event> getAllEvent() {
+       return eventRepo.findAll();
+    }
+
+    @Cacheable(value = "events" , key = "#EventId")
+    public Event EventById(Long EventId) {
+        return  eventRepo.findById(EventId).orElseThrow(() -> new EntityNotFoundException("Event Not Found"));
+    }
 }
