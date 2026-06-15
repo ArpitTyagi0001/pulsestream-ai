bb8d4a464c558d6eece1b26204f949237d1c55ca
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-15 21:31:25 +0530

    Add the Api

--- backend/query_service/src/main/java/com/streamanalytics/query_service/controller/EventController.java
+++ backend/query_service/src/main/java/com/streamanalytics/query_service/controller/EventController.java
@@ -2,15 +2,13 @@ package com.streamanalytics.query_service.controller;
 
 import com.streamanalytics.query_service.entity.Event;
 import com.streamanalytics.query_service.service.EventService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/events")
+@RequestMapping("/api/dashboard")
 public class EventController {
     private EventService eventService;
 
@@ -18,13 +16,24 @@ public class EventController {
         this.eventService = eventService;
     }
 
-    @GetMapping("/all")
-    public List<Event> getAllEvent(){
-        return eventService.getAllEvent();
+    @GetMapping("/all-events")
+    public List<Event> allEvent(){
+        return eventService.getEvent();
     }
 
-    @GetMapping("/get/{EventId}")
-    public Event getEventById(@PathVariable Long EventId){
-        return eventService.EventById(EventId);
+    @GetMapping("/get/{eventId}")
+    public ResponseEntity<Event> EventById(@PathVariable Long eventId){
+        return ResponseEntity.ok(eventService.getEventId(eventId));
     }
+
+    @GetMapping("/total-events")
+    public long totalEvent(){
+        return eventService.count();
+    }
+
+    @GetMapping("/latest-events")
+    public Event latestEvent(){
+        return  eventService.getLatest();
+    }
+
 }
