5781bc6e00f3a611e1f91492a24d242eb9280994
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-15 21:33:08 +0530

    Add Many Apis

--- backend/event-producer-service/src/main/java/com/streamanalytics/event_service/controller/LocationController.java
+++ backend/event-producer-service/src/main/java/com/streamanalytics/event_service/controller/LocationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/location")
+@RequestMapping("/api/location")
 public class LocationController {
 
     private final KafkaService kafkaService;
