162e7409123aa9d8b19f654034eae729f55d89f1
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-17 21:46:11 +0530

    Add the Manageable query to find top 10 latest event

--- backend/query_service/src/main/java/com/streamanalytics/query_service/repo/EventRepo.java
+++ backend/query_service/src/main/java/com/streamanalytics/query_service/repo/EventRepo.java
@@ -4,6 +4,10 @@ import com.streamanalytics.query_service.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepo extends JpaRepository<Event, Long> {
+
+    List<Event> findTop10ByOrderByEventAtDesc();
 }
