1f37c32b7ff85e42ac7d831b567451b371b723a0
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-18 22:18:59 +0530

    Add the controller layer and apis to access data and make it summary

---
+++ backend/spring-ai/src/main/java/com/streamanalytics/spring_ai/controller/AiController.java
@@ -0,0 +1,28 @@
+package com.streamanalytics.spring_ai.controller;
+
+import com.streamanalytics.spring_ai.service.AiService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ai")
+public class AiController {
+
+    private AiService aiService;
+
+    public AiController(AiService aiService) {
+        this.aiService = aiService;
+    }
+
+    @GetMapping("/chat-box")
+    public String chatBox(@RequestParam String message){
+        return aiService.getChat(message);
+    }
+
+    @GetMapping("/event-summary")
+    public String Summary(){
+        return aiService.Summary();
+    }
+}
