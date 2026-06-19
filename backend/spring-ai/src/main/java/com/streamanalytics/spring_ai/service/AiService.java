5f895f359b7f6a023158babcfe568bc7178cb282
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-19 22:13:07 +0530

    Add the more api call to ai service to query service and now make perfect summary

--- backend/spring-ai/src/main/java/com/streamanalytics/spring_ai/service/AiService.java
+++ backend/spring-ai/src/main/java/com/streamanalytics/spring_ai/service/AiService.java
@@ -1,8 +1,11 @@
 package com.streamanalytics.spring_ai.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+import java.util.Map;
 
 @Service
 public class AiService {
@@ -26,18 +29,31 @@ public class AiService {
                 .uri("http://localhost:8083/api/dashboard/total-events")
                 .retrieve()
                 .body(Long.class);
+        
+        String latestEvents = restClient.get()
+                .uri("http://localhost:8083/api/dashboard/latest-events")
+                .retrieve()
+                .body(String.class);
+
+        Map<String , Long> locationCount = restClient.get()
+                .uri("http://localhost:8083/api/dashboard/location-count")
+                .retrieve()
+                .body(new ParameterizedTypeReference<Map<String, Long>>(){});
 
         String message = """
 
         You are an analytics assistant.
 
         Total Events: %d
+        
+        Latest Events: %s
+        
+        Location Events: %s
 
-        Based only on this number, generate a short dashboard summary in 2-3 lines.
-
-        Do not ask for more information.
-
-        """.formatted(totalEvents);
+        Generate a professional dashboard summary in 4-5 lines.
+        Mention trends and active locations.
+        Do not ask questions.
+        """.formatted(totalEvents , latestEvents , locationCount);
 
         return chatClient.prompt()
                 .user(message)
