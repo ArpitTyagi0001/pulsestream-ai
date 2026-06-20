6970bad954b6d63fbd582a1373aea931f7c4b6e7
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-20 23:16:15 +0530

    make users Dto to secure or hide data

---
+++ backend/auth-service/src/main/java/com/streamanalytics/auth_service/dto/UsersDto.java
@@ -0,0 +1,13 @@
+package com.streamanalytics.auth_service.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+public class UsersDto {
+    private String message;
+    private String username;
+}
