3d6093aa1d707c50044120f75a84faabe8723c56
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-21 22:20:01 +0530

    Make login api logic

--- backend/auth-service/src/main/java/com/streamanalytics/auth_service/service/UsersService.java
+++ backend/auth-service/src/main/java/com/streamanalytics/auth_service/service/UsersService.java
@@ -3,6 +3,10 @@ package com.streamanalytics.auth_service.service;
 import com.streamanalytics.auth_service.dto.UsersDto;
 import com.streamanalytics.auth_service.entity.Users;
 import com.streamanalytics.auth_service.repo.UsersRepo;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +14,12 @@ import org.springframework.stereotype.Service;
 public class UsersService {
     private final UsersRepo usersRepo;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
-    public UsersService(UsersRepo usersRepo, PasswordEncoder passwordEncoder) {
+    public UsersService(UsersRepo usersRepo, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.usersRepo = usersRepo;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
     }
 
     public UsersDto register(Users users) {
@@ -22,5 +28,14 @@ public class UsersService {
         Users savedUser = usersRepo.save(users);
 
         return new UsersDto("User registered successfully" , savedUser.getUsername());
+    }
+
+    public UsersDto login(Users users) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(users.getUsername(), users.getPassword()));
+            return new UsersDto("User login successfully" , users.getUsername());
+        }catch (BadCredentialsException e){
+           throw new RuntimeException("Invalid username and password");
+        }
     }
 }
