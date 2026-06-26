d136f3bd9d6bed10139d8d5ae4d909556f56dcc1
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-26 23:49:10 +0530

    Change the login funcation

--- backend/auth-service/src/main/java/com/streamanalytics/auth_service/service/UsersService.java
+++ backend/auth-service/src/main/java/com/streamanalytics/auth_service/service/UsersService.java
@@ -3,6 +3,9 @@ package com.streamanalytics.auth_service.service;
 import com.streamanalytics.auth_service.dto.UserDto;
 import com.streamanalytics.auth_service.model.Users;
 import com.streamanalytics.auth_service.repo.UsersRepo;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,25 +13,31 @@ import org.springframework.stereotype.Service;
 public class UsersService {
     private final UsersRepo usersRepo;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JWTService jwtService;
 
-    public UsersService(UsersRepo usersRepo, PasswordEncoder passwordEncoder) {
+    public UsersService(UsersRepo usersRepo, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, JWTService jwtService) {
         this.usersRepo = usersRepo;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
-    public UserDto userRegister(Users users) {
-       users.setPassword(passwordEncoder.encode(users.getPassword()));
-       Users user = usersRepo.save(users);
+    public UserDto userRegister(Users user) {
+       user.setPassword(passwordEncoder.encode(user.getPassword()));
+       Users user1 = usersRepo.save(user);
        return new UserDto("User Successfully Register" , user.getUsername());
     }
 
-    public boolean userLogin(Users users) {
-        Users user = usersRepo.findByUsername(users.getUsername());
+    public String userLogin(Users user) {
+        Authentication authentication =
+                authenticationManager
+                        .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
 
-        if(user == null){
-            return false;
+        if(authentication.isAuthenticated()){
+             return jwtService.generateToken(user.getUsername());
         }
 
-        return true;
+        return "fail";
     }
 }
