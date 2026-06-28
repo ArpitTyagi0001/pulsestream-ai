93b9c52a05b7ce4e03f90c205418a16e088fd7d5
Arpit Tyagi <arpittyagi389@gmail.com>
2026-06-28 23:22:05 +0530

    Add the exclude the userdetailsservice autoconfiguration

--- backend/spring-ai/src/main/java/com/streamanalytics/spring_ai/SpringAiApplication.java
+++ backend/spring-ai/src/main/java/com/streamanalytics/spring_ai/SpringAiApplication.java
@@ -2,8 +2,9 @@ package com.streamanalytics.spring_ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class SpringAiApplication {
 
 	public static void main(String[] args) {
