87c03ac0ad3324a485a1a803e0f091dd77f92b38
Arpit Tyagi <arpittyagi389@gmail.com>
2026-07-05 18:18:25 +0530

    Correct spring-si service security package

--- backend/spring-ai/src/main/java/com/streamanalytics/spring_ai/config/SecurityConfig.java
+++ backend/spring-ai/src/main/java/com/streamanalytics/spring_ai/config/SecurityConfig.java
@@ -6,16 +6,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private final JwtFilter jwtFilter;
-
-    public SecurityConfig(JwtFilter jwtFilter) {
-        this.jwtFilter = jwtFilter;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,7 +19,6 @@ public class SecurityConfig {
                         request.anyRequest().authenticated())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtFilter , UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
