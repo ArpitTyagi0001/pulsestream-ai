eba581573b8525b9e013211453ba655bca3b2962
Arpit Tyagi <arpittyagi389@gmail.com>
2026-07-05 18:14:08 +0530

    Correct the security service of the query service

--- backend/query_service/src/main/java/com/streamanalytics/query_service/config/SecurityConfig.java
+++ backend/query_service/src/main/java/com/streamanalytics/query_service/config/SecurityConfig.java
@@ -6,27 +6,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final JwtFilter jwtFilter;
-
-    public SecurityConfig(JwtFilter jwtFilter) {
-        this.jwtFilter = jwtFilter;
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf((csrf -> csrf.disable()))
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .authorizeHttpRequests(request -> request.anyRequest()
+                        .permitAll())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtFilter , UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
