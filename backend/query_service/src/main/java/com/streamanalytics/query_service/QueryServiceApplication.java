c26f0fd6b064b958e2571bedb4800d0cc9f5cebf
Arpit Tyagi <arpittyagi389@gmail.com>
2026-07-14 20:04:32 +0530

    Add Dockerfiles and update service configuration

--- backend/query_service/src/main/java/com/streamanalytics/query_service/QueryServiceApplication.java
+++ backend/query_service/src/main/java/com/streamanalytics/query_service/QueryServiceApplication.java
@@ -3,8 +3,10 @@ package com.streamanalytics.query_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@EnableCaching
 public class QueryServiceApplication {
 
 	public static void main(String[] args) {
